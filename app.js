c182a3eac289626aa53a6f4cccfef0d5ad1bdd61
dbojantchev <dimitar@pontix.com>
2015-11-28 19:15:51 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var api;
+var DATABASE = 'SqlServer';
+
+if(DATABASE === 'SqlServer') {
+    api = require('./server/routes/apiRouteSqlServer');
+}
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.engine('html', require('ejs').renderFile);
+
+//uncomment after placing your favicon in /web
+//app.use(favicon(__dirname + '/web/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'web')));
+
+app.use('/api', api);
+
+console.log('app.mountpath=' + app.mountpath);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
